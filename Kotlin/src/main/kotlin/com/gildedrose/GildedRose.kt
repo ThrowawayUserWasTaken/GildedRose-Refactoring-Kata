dc0428fc06a4349805b613a5bc340392b74d3836
ThrowawayUserWasTaken <43199223+ThrowawayUserWasTaken@users.noreply.github.com>
2024-11-06 18:12:12 +0100

    Apply code suggestions

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,23 +7,23 @@ class GildedRose(var items: List<Item>) {
             if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (item.quality > 0) {
                     if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
+                        item.quality -= 1
                     }
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality += 1
 
                     if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.quality += 1
                             }
                         }
 
                         if (item.sellIn < 6) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.quality += 1
                             }
                         }
                     }
@@ -31,7 +31,7 @@ class GildedRose(var items: List<Item>) {
             }
 
             if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
+                item.sellIn -= 1
             }
 
             if (item.sellIn < 0) {
@@ -39,7 +39,7 @@ class GildedRose(var items: List<Item>) {
                     if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (item.quality > 0) {
                             if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
+                                item.quality -= 1
                             }
                         }
                     } else {
@@ -47,7 +47,7 @@ class GildedRose(var items: List<Item>) {
                     }
                 } else {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.quality += 1
                     }
                 }
             }
