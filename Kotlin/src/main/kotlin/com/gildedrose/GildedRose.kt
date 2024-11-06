9392395fdbde14be6623a781ac8daa0672d228fb
ThrowawayUserWasTaken <43199223+ThrowawayUserWasTaken@users.noreply.github.com>
2024-11-06 20:04:54 +0100

    Clean up updateQuality

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -8,73 +8,13 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         for (item in items) {
             when (item.name) {
-                AGED_BRIE_NAME -> {
-                    updateAgedBrie(item)
-                    continue
-                }
-
-                BACKSTAGE_PASS_NAME -> {
-                    updateBackstagePass(item)
-                    continue
-                }
-
+                AGED_BRIE_NAME -> updateAgedBrie(item)
+                BACKSTAGE_PASS_NAME -> updateBackstagePass(item)
                 SULFURAS_NAME -> {
                     // The Sulfuras, Hand of Ragnaros is pure. Nothing changes
-                    continue
                 }
 
-                else -> {
-                    updateGenericItem(item)
-                    continue
-                }
-            }
-
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality -= 1
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality += 1
-
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality += 1
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality += 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn -= 1
-            }
-
-            if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality -= 1
-                            }
-                        }
-                    } else {
-                        item.quality = 0
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality += 1
-                    }
-                }
+                else -> updateGenericItem(item)
             }
         }
     }
