ece6ee492b54c1b4152ddd2cad0197365a492b09
ThrowawayUserWasTaken <43199223+ThrowawayUserWasTaken@users.noreply.github.com>
2024-11-07 19:27:51 +0100

    Add the "mother test"

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -2,9 +2,6 @@ package com.gildedrose
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 
 internal class GildedRoseTest {
 
@@ -14,5 +11,37 @@ internal class GildedRoseTest {
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals(TEST_ITEM_NAME, app.items[0].name)
+    }
+
+    @Test
+    fun `When we update the quality of Gilded Rose, we update the quality of all its items`() {
+        // Given
+        val genericItem = Item(name = "we-do-not-care", sellIn = 12, quality = 4)
+        val backstagePass = Item(name = BACKSTAGE_PASS_NAME, sellIn = 5, quality = 10)
+        val sulfurasItem = Item(name = SULFURAS_NAME, sellIn = 2024, quality = SULFURAS_QUALITY)
+        val agedBrie = Item(name = AGED_BRIE_NAME, sellIn = 7, quality = 48)
+        val items = listOf(genericItem, backstagePass, sulfurasItem, agedBrie)
+
+        // When
+        val gildedRose = GildedRose(items)
+        gildedRose.updateQuality()
+
+        // Then
+        val expectedItems = listOf(
+            genericItem.apply {
+                updateGenericItemQuality()
+            },
+            backstagePass.apply {
+                updateBackstagePassQuality()
+            },
+            sulfurasItem.apply {
+                verifySulfuras()
+            },
+            agedBrie.apply {
+                updateAgedBrieQuality()
+            },
+        )
+        // TODO: wait, why does this work again? These are real classes, no data classes. Ah, well, for later
+        assertEquals(expectedItems, gildedRose.items)
     }
 }
