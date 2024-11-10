ac9b0361ab0b6adf1a9fa7cbe1a774e67e51d576
ThrowawayUserWasTaken <43199223+ThrowawayUserWasTaken@users.noreply.github.com>
2024-11-10 18:05:24 +0100

    Refactor to remove double else

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,16 +4,14 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.isConjured) {
-                item.updateConjuredItemQuality()
-            } else {
-                when (item.name) {
-                    AGED_BRIE_NAME -> item.updateAgedBrieQuality()
-                    BACKSTAGE_PASS_NAME -> item.updateBackstagePassQuality()
-                    // We only verify Sulfuras, Hand of Ragnaros
-                    SULFURAS_NAME -> item.verifySulfuras()
-                    else -> item.updateGenericItemQuality()
-                }
+            val name = item.name
+            when {
+                item.isConjured -> item.updateConjuredItemQuality()
+                name == AGED_BRIE_NAME -> item.updateAgedBrieQuality()
+                name == BACKSTAGE_PASS_NAME -> item.updateBackstagePassQuality()
+                // We only verify Sulfuras, Hand of Ragnaros
+                name == SULFURAS_NAME -> item.verifySulfuras()
+                else -> item.updateGenericItemQuality()
             }
         }
     }
